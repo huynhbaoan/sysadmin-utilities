9ee9cec480b00813eb923443e125bf454932405f
Huynh Bao An <an.hb@gnt.com.vn>
2018-06-21 16:58:36 +0700

    Delete old file  On branch master  Changes to be committed: 	deleted:    newfile_scan.sh 	deleted:    svn/svnlist.sh 	deleted:    svndump.sh 	deleted:    svnlist.sh

--- svndump.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-while read LINE
-do
-    /usr/bin/svnadmin dump --deltas /svn/"$LINE"/ | gzip -5 > /svnbackup/"$LINE".dump.gz 2> /root/anhuynh/svndump.err
-done < /root/anhuynh/svndump.list
